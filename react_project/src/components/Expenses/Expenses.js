c69bab6f4bc52a28198a1bcdb490c6f9d8e8efb6
loretari <loretari@gmail.com>
2023-03-25 10:38:08 +0200

    init: add conditional Return Statements

--- react_project/src/components/Expenses/Expenses.js
+++ react_project/src/components/Expenses/Expenses.js
@@ -1,8 +1,9 @@
 import React, { useState} from 'react'
 import Card from '../UI/Card'
 import './Expenses.css';
-import ExpenseItem from "./ExpenseItem";
+import ExpensesList from './ExpensesList'
 import ExpensesFilter from "./ExpensesFilter";
+
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2023')
@@ -17,35 +18,23 @@ const Expenses = (props) => {
 
 
 
-// let expensesContent = <p>No expenses found!</p>
-//
-//     if (filteredExpenses.length > 0) {
-//         filteredExpenses.map((expense) => (
-//             <ExpenseItem
-//                 key={expense.id}
-//                 title={expense.title}
-//                 amount={expense.amount}
-//                 date={expense.date}
-//             />
-//         ))
-//     }
     return (
         <div>
 
         <Card className='expenses'>
             <ExpensesFilter selected={filteredYear} onChangeFilter = {filterChangeHandler}/>
-            {/*{expensesContent}*/}
-            {filteredExpenses.length === 0 && <p>No expenses found!</p>}
-            {filteredYear.length > 0 &&
-            filteredExpenses.map((expense) => (
-                <ExpenseItem
-                    key={expense.id}
-                    title={expense.title}
-                    amount={expense.amount}
-                    date={expense.date}
-                />
-            ))
-            }
+           <ExpensesList items={filteredExpenses}/>
+            {/*{filteredExpenses.length === 0 && <p>No expenses found!</p>}*/}
+            {/*{filteredYear.length > 0 &&*/}
+            {/*filteredExpenses.map((expense) => (*/}
+            {/*    <ExpenseItem*/}
+            {/*        key={expense.id}*/}
+            {/*        title={expense.title}*/}
+            {/*        amount={expense.amount}*/}
+            {/*        date={expense.date}*/}
+            {/*    />*/}
+            {/*))*/}
+            {/*}*/}
 
 
 
